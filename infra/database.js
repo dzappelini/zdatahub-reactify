3b846922f0b5329dd7ed612a206483c550c13a64
Douglas Zappelini Neto <61122920+dzappelini@users.noreply.github.com>
2024-05-21 18:36:41 +0000

    working on migrations and tests

--- infra/database.js
+++ infra/database.js
@@ -1,25 +1,65 @@
 import { Client } from "pg";
 
+// for use with aiven.io
+// const sslCA = Buffer.from(
+//   process.env.POSTGRES_SSL_CA_BASE64,
+//   "base64",
+// ).toString("utf-8");
+
 async function query(queryObject) {
-  const client = new Client({
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-  });
+  let client;
+
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
-  } catch (err) {
-    console.error("Database query error:", err);
-    throw err;
+  } catch (error) {
+    console.error("Database query error:", error);
+    throw error;
   } finally {
     await client.end();
   }
 }
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
+}
+
 export default {
   query,
+  getNewClient,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "production" ? true : false;
+}
+
+// console.log(
+//   "host: ",
+//   process.env.POSTGRES_HOST,
+//   "port: ",
+//   process.env.POSTGRES_PORT,
+//   "user: ",
+//   process.env.POSTGRES_USER,
+//   "database: ",
+//   process.env.POSTGRES_DB,
+//   "password: ",
+//   process.env.POSTGRES_PASSWORD,
+//   "ssl: ",
+//   getSSLValues(),
+// );
--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,36 +1,39 @@
-const migrationRunner = require("node-pg-migrate").default;
+// const migrationRunner = require("node-pg-migrate").default;
 import { join } from "node:path";
-import { Client } from "pg"; // Importar o Client corretamente
+import { Client } from "pg";
+import migrationRunner from "node-pg-migrate";
 
 export default async function migrations(request, response) {
   const databaseUrl = process.env.DATABASE_URL;
   const client = new Client({ connectionString: databaseUrl });
 
+  const defaultMigrationOptions = {
+    dbClient: client, // Passar o cliente conectado
+    dryRun: true,
+    dir: join("infra", "migrations"), // Usar caminho absoluto
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
   try {
     await client.connect();
 
     if (request.method === "GET") {
-      const migrations = await migrationRunner({
-        dbClient: client, // Passar o cliente conectado
-        dryRun: true,
-        dir: join("infra", "migrations"), // Usar caminho absoluto
-        direction: "up",
-        verbose: true,
-        migrationsTable: "pgmigrations",
-      });
-      return response.status(200).json(migrations);
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      return response.status(200).json(pendingMigrations);
     }
 
     if (request.method === "POST") {
-      const migrations = await migrationRunner({
-        dbClient: client, // Passar o cliente conectado
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
         dryRun: false,
-        dir: join("infra", "migrations"), // Usar caminho absoluto
-        direction: "up",
-        verbose: true,
-        migrationsTable: "pgmigrations",
       });
-      return response.status(200).json(migrations);
+
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+      return response.status(200).json(migratedMigrations);
     }
 
     return response.status(405).end();
